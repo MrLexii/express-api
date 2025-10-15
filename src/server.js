5b42b418b837b1152a07d5a038af680782ce07fb
Felix Pinson <felixpinson@etu.unicaen.fr>
2025-10-15 14:54:58 +0200

    serv premier partie fini 15/1/25

--- src/server.js
+++ src/server.js
@@ -1,12 +1,43 @@
-import http from 'http'
+import express from 'express'
 
-const port = process.env.POST || 3000
+const port = process.env.PORT || 3000
 
-const server = http.createServer((req, res)=> {
-    res.end('SSSSSSSSSSSSALUT QUONNART !')
+const app = express()
+
+app.use(express.json())
+
+app.get('/questions', (req, res)=>{
+    res.status(200).send([
+        {
+            id: "1",
+            question: "kappytalle phransse?",
+            answer: 'paris',
+        }
+    ])
 })
 
-server.listen(port, ()=> {
-    console.log(`serveur running on http://localhost:${port}`)
+app.post('/questions',(req, res)=>{
+    const{ question, answer} = req.body
+
+    if(!question || !answer){
+        res.status(400).send({
+            error: "question et reponse requis",
+        })
+    }
+
+    res.status(201).send({
+        message:"question crée avec succes",
+    })
 })
 
+app.delete('/questions/:id', (req, res)=>{
+    const {id} = req.params
+
+    res.status(200).send({
+        message:`keçtion ${id} çupprymais`
+    })
+})
+
+app.listen(port, ()=>{
+    console.log(`server run on http://localhost${port}`)
+})
