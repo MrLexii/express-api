c8647d535bd5b49d41b942c2ec0dfc1c428da89e
Felix Pinson <felixpinson@etu.unicaen.fr>
2025-11-19 14:57:54 +0100

    login v2 (pas complet)

---
+++ src/middleware/authToken.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
+
+export const authToken = (req, res, next)=> {
+    const authHeader = req.headers.authorization
+    const token = authToken && authHeader.split(' ')[1]
+
+    if(!token){
+        return res.status(403).json({error : " acces token required"})
+    }
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+        next()
+    } catch (error) {
+        console.error(error)
+        res.status(401).json({error:"invalid or expired token"})
+    }
+}
